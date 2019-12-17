32ba81b3140006816e1a32ec95e0ada5c0695b8b
Anita Kemp <anita.k.kemp@gmail.com>
2019-12-17 13:21:45 -0800

    Add paragraph to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,33 @@ import React from 'react';
 import language from '../Assets/language.jpg'
 
 function Header() {
-    var textStyle={
+    var coverImage={
+        width: '100%',
+    }
+    var heroText = {
+        textAlign: "center",
+        position: "absolute",
+        top: "19%",
+        right: "35%",
+        fontSize: "80px",
         color: '#4056A1'
+
     }
 
-    var coverImage={
-        width: '100%'
+    var heroInfo = {
+        textAlign: "center",
+        position: "absolute",
+        bottom: "42%",
+        left: "35%",
+        fontSize: "20px",
+        color: '#4056A1'
+
     }
     return (
         <div>
             <img src={language} style={coverImage} />
-            <h1 style={textStyle}>Welcome to the corpus of something...</h1>
+            <h1 style={heroText}>Welcome to the corpus of something...</h1>
+            <p style={heroInfo}>A place for the public and researchers to add to a language data set</p>
         </div>
     );
 }
